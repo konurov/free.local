0467939f2e6db1a34f049d2338dc6585e2687213
Konurov <s_konurov@mail.ru>
2025-02-08 14:23:33 +0600

    my

--- index.php
+++ index.php
@@ -6,6 +6,68 @@
     <title>Document</title>
 </head>
 <body>
-    <h2>Free.local</h2>
+    <?php 
+    /*for($i = 1; $i<100; $i++){
+        echo "<h1>free</h1>";
+    }*/
+    #sanjar
+    //sanjar
+
+    //https://itproger.com/course/php-mysql/2
+
+   #3 – Переменные и типы данных в PHP
+   //https://itproger.com/course/php-mysql/3
+
+   /*$d1 = true;
+
+   echo var_dump($d1);*/
+
+  /* $d1 = "Sanjar";
+   echo var_dump($d1);
+
+   $first_num = 23.2; // Тип данных float
+
+   echo var_dump($first_num);
+   $first_num = "1"; // Тип данных string
+   echo "<br>";
+
+   define("HTTPS", "http://spravka.arabaev.kg/");
+   echo HTTPS; // Вывод константы
+   
+   #4 – Математические действия. Встроенные функции
+   //https://itproger.com/course/php-mysql/4
+   
+   
+   /*вычитание - $z = $y - $x;
+сложение - $z = $y + $x;
+умножение - $z = $y * $x;
+деление - $z = $y / $x (на ноль делить нельзя);
+остаток при делении - $z = $y % $x;*/
+
+#$z = 5 + 5;
+            $y = 5;
+            $x = 20;
+            $z=$y-$x;
+            //echo $z;
+
+            //echo --$x;
+
+            $z = 50;
+            //$z = $z + 20;
+           
+            $z += 20; // Эта запись аналогична той, что находиться выше
+
+                $z = 10;
+                $z += 1;
+                $z--; // Аналогично тому, что прописано выше
+
+            echo $z;
+
+
+
+
+
+    ?>
+    
 </body>
 </html>
