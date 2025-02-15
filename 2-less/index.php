9a32a411770db0aa63c6846f4fd68c218e6a8ae4
Konurov <s_konurov@mail.ru>
2025-02-15 14:20:03 +0600

    m

---
+++ 2-less/index.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php 
+error_reporting(-1);
+/*
+Арифметически операторы
+"+" сложение $a + $b
+"-" вычитание $a - $b
+"*" умножение $a * $b
+"/" деление $a / $b
+"-$a" отрицание (смена знака $a)
+"$a % $b" деление по модулю (остаток от деления)
+"$a ** $b" возведение в степень
+"=" присваивание (установка значения)
+"&" присваивание по ссылке
+============================
+"++$a" префиксный инкремент
+"$a++" постфиксный инкремент
+"--$a" префиксный декремент
+"$a++" постфиксный декремент
+"." конкатенация (склеивание строк)
+комбинированные операторы
+*/
+
+$a = 5;
+$b = 2;
+$c = $a ** $b;
+
+//echo '<h1>$c</h1>';
+/*
+$a = $a + 1; // $a++
+var_dump($a);
+
+//$str1 = 'Hello,';
+$str2 = ' world';
+$str = 'Hello,' . $str2;
+echo $str;
+
+ $fruit = 'apple';
+$winnie_pooh = "Hello, I\"m Winnie. I have 2 {$fruit}s ";
+
+//echo $winnie_pooh ;
+echo $winnie_pooh = 'Hello, I"m Winnie. I have 2 ' . $fruit . 's';
+
+$a = 5;
+$a = $a + 2;
+$a += 2;
+$a *= 2;
+var_dump($a);
+
+$str1 = 'Hello,';
+echo $str1 .= ' world';*/
+
+//NULL;
+
+//$var = '';
+// var_dump($var); // null
+//$var = '';
+//unset($var);
+//$var = NULL;
+//$var++;
+//var_dump($var);
+
+/*if(expression){
+	instructions;
+}*/
+
+//$var = false;
+
+//echo $var;
+//var_dump($var);
+//var_dump((bool) 0);
+// var_dump((bool) '');
+
+/*$light = 'red';
+
+if( $light != 'green' ){
+	echo 'We may go';
+}*/
+
+// var_dump( 5 < 3 );
+
+
+if( 5 != 5 ){
+	echo 'OK';
+}
+
+
+/*if( 5 != 4 ){
+	echo 'OK';
+	echo '<p>2</p>';
+}*/
+
+
+
+
+
+
+
+
+
+?>
+
+
+</body>
+</html>
+
+
